1688e035a7cafa41ce1760a753be45300103a093
Owner <vtsoneva@gmail.com>
2020-02-19 17:40:44 +0200

    small edits

--- dist/style.9de6a377.js
+++ dist/style.9de6a377.js
@@ -194,7 +194,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./utils/variables.css":"zoi/utils/variables.css","_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./utils/variables.css":"zoi/utils/variables.css","./assets/arrow.svg":[["arrow.eb6d1997.svg","zoi/assets/arrow.svg"],"zoi/assets/arrow.svg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -222,7 +222,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49453" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61420" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
